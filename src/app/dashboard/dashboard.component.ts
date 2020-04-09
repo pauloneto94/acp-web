3a29fa5f552f331b8085d39cc14ce6cac1ef14cc
pauloneto94 <pauloneto913@gmail.com>
2020-04-09 18:21:22 -0300

    add upload to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { NoticiaService } from '../services/noticia.service'
 import { Noticia } from '../model/noticias';
 import { NgForm } from '@angular/forms';
 import { format } from 'url';
+import { FirestorageService } from '../services/firestorage.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,7 +13,7 @@ import { format } from 'url';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private noticiaService: NoticiaService) { }
+  constructor(private noticiaService: NoticiaService, private firestorageService: FirestorageService) { }
 
   noticia: Noticia; 
 
@@ -23,6 +25,19 @@ export class DashboardComponent implements OnInit {
     this.noticiaService.addNew(this.noticia);
   }
 
+  showPreview(event: any) {
+    this.firestorageService.setImage(event.target.files[0])
+    this.firestorageService.save().snapshotChanges().pipe(
+      finalize(() => {
+        this.firestorageService.storageRef.getDownloadURL().subscribe(downloadURL => {
+          console.log('File available at', downloadURL);
+          this.noticia.image = downloadURL;
+        });
+      })
+    ).subscribe();
+  }
+
   get diagnostic() { return JSON.stringify(this.noticia); }
+
 
 }
